d0244cbf100839608d166b269b66420cc475be5a
gabi <144284968+ninikx@users.noreply.github.com>
2023-10-22 19:11:53 +0200

    delete duplicate mapping

--- .config/nvim/lua/user/mappings.lua
+++ .config/nvim/lua/user/mappings.lua
@@ -17,10 +17,6 @@ return {
       function() require('astronvim.utils.buffer').nav(-(vim.v.count > 0 and vim.v.count or 1)) end,
       desc = 'Previous buffer',
     },
-    Q = {
-      function() require('astronvim.utils.buffer').close() end,
-      desc = 'Close current buffer',
-    },
 
     -- mappings seen under group name 'Buffer'
     ['<leader>bD'] = {
