e00b929ba58fbd6003c2cf8fcf409cab78c65d96
gabi <gabriel@nagelmichaelsen.de>
2023-12-18 21:00:17 +0100

    format lua files

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -1,16 +1,16 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+	-- Add the community repository of plugin specifications
+	"AstroNvim/astrocommunity",
+	-- example of importing a plugin, comment out to use it or add your own
+	-- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.pack.rust" },
-  { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.pack.java" },
-  { import = "astrocommunity.pack.python" },
-  { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.markdown-and-latex.vimtex" },
-  { import = "astrocommunity.project.project-nvim" },
-  { import = "astrocommunity.motion.mini-move" },
-  { import = "astrocommunity.motion.mini-surround" },
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.lua" },
+	{ import = "astrocommunity.pack.java" },
+	{ import = "astrocommunity.pack.python" },
+	{ import = "astrocommunity.pack.cpp" },
+	{ import = "astrocommunity.markdown-and-latex.vimtex" },
+	{ import = "astrocommunity.project.project-nvim" },
+	{ import = "astrocommunity.motion.mini-move" },
+	{ import = "astrocommunity.motion.mini-surround" },
 }
