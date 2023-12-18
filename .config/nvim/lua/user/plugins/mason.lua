cd78a75af96f1777be304a42d29a27432c19b3f8
gabi <gabriel@nagelmichaelsen.de>
2023-12-18 21:00:04 +0100

    format

--- .config/nvim/lua/user/plugins/mason.lua
+++ .config/nvim/lua/user/plugins/mason.lua
@@ -1,35 +1,35 @@
 return {
   {
-    'williamboman/mason-lspconfig.nvim',
+    "williamboman/mason-lspconfig.nvim",
     -- overrides `require('mason-lspconfig').setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require('astronvim.utils').list_insert_unique(opts.ensure_installed, {
-        'lua_ls',
-        'clangd',
-        'bashls',
-        'jdtls',
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
+        "lua_ls",
+        "clangd",
+        "bashls",
+        "jdtls",
       })
     end,
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
-    'jay-babu/mason-null-ls.nvim',
+    "jay-babu/mason-null-ls.nvim",
     -- overrides `require('mason-null-ls').setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require('astronvim.utils').list_insert_unique(opts.ensure_installed, {
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- 'prettier',
         -- 'stylua',
       })
     end,
   },
   {
-    'jay-babu/mason-nvim-dap.nvim',
+    "jay-babu/mason-nvim-dap.nvim",
     -- overrides `require('mason-nvim-dap').setup(...)`
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
-      opts.ensure_installed = require('astronvim.utils').list_insert_unique(opts.ensure_installed, {
+      opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- 'python',
       })
     end,
